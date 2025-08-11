c6de23f587cbabbf298b62f79dc2e3f2faafa21b
GuintherBr <jeffersonguinther@gmail.com>
2025-08-11 20:41:53 -0300

    ate aqui as funcoes estava separadas

---
+++ README.md
@@ -0,0 +1,11 @@
+# programadordesistema
+cd pi_react
+npx create-react-app .
+
+# para entrar na pasta pi_react
+cd pi_react
+
+# se nao tiver aparecendo a node modules falta
+npm install
+
+
--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App() { //aqui é javascript
+ // var oi1  // var é igual variavel global, let é local
+
+  //const oi2 // const é uma variavel que não pode ser alterada constante
+
+  let oi = "Hello, World!"; // let é uma variavel local
+
+
+  //oi = oi + " Jefferson"; //soma de string
+  //oi += " Jefferson"; //soma suprimida
+
+  function soma(a, b) {// a e b são parâmetros da função 
+    return a + b; //função que retorna a soma de dois números
+  }
+
+  function divide (a, b) {
+    return a / b; //função que retorna a divisão de dois números
+  }
+
+  return ( /*aqui é html*/
+    
+    <main className="App">
+      {soma (oi, "Jefferson")} <br/>
+      {divide(36, 6)} <br/>
+    </main>
+
   );
 }
 
