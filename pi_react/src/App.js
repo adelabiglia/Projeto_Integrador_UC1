ebeced4fcd351d09287cb7a0382c1e6fb3da0e11
GuintherBr <jeffersonguinther@gmail.com>
2025-08-11 21:09:42 -0300

    calculadora

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,30 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() { //aqui é javascript
- // var oi1  // var é igual variavel global, let é local
-
-  //const oi2 // const é uma variavel que não pode ser alterada constante
-
-  let oi = "Hello, World!"; // let é uma variavel local
-
-
-  //oi = oi + " Jefferson"; //soma de string
-  //oi += " Jefferson"; //soma suprimida
-
-  function soma(a, b) {// a e b são parâmetros da função 
-    return a + b; //função que retorna a soma de dois números
+function App() { // aqui é o componente principal do React, 
+// onde você pode definir a estrutura da sua aplicação.
+  
+function calculadora(a,b,op) {
+  if (op == '+') {
+    return a + b;
+  }else if (op == '-') {
+    return a - b;
+  } else if (op == '*') {
+    return a * b;
+  } else if (op == '/') {
+    return a / b;
   }
-
-  function divide (a, b) {
-    return a / b; //função que retorna a divisão de dois números
-  }
-
-  return ( /*aqui é html*/
+}
+  return ( 
     
     <main className="App">
-      {soma (oi, "Jefferson")} <br/>
-      {divide(36, 6)} <br/>
+
+      {calculadora(5, 10, '+')} <br/> 
+      {calculadora(6, 3, '-')} <br/> 
+      {calculadora(5, 2, '*')} <br/> 
+      {(calculadora(14, 3, '/').toFixed(2))} <br/> 
+      {calculadora(10, calculadora(2, 3,'+'), '*')} <br/>
+      
+  
     </main>
 
   );
