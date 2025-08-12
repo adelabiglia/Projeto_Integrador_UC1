7b1f3c80d520e9f42d4b3af44de3309ea7c78f23
Helena-Siqueira <156859073+Helena-Siqueira@users.noreply.github.com>
2025-08-11 22:26:59 -0300

    Exercicio vetor impares

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -42,14 +42,26 @@ function App() { // Aqui é JavaScript//
       return a * b;
     } */ 
 
-    let cesta = ['pão', 'suco', 'fruta', 'feijão']
+    /*let cesta = ['pão', 'suco', 'fruta', 'feijão']
   
     let desenha = [];
     // Declara o índice; compara se é para continuar; incrementa o índice
     for(let i=0; i<6; i++){
       desenha.push(<p>  { cesta[i] } </p>)
 
-    }
+    } */
+
+    // Faça uma lista como todos os números até o 10, mas exiba apenas os número impares
+
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  
+    let desenha = [];
+
+    for(let i=0; i<10; i++ ){
+      if(numeros[i] % 2 !== 0){
+        desenha.push(<p> {numeros[i]} </p>)
+      }
+    } 
 
     return ( /* Aqui é HTML */
       <main className="App">
