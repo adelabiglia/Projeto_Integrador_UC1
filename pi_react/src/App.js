eadc7677b5e5ca71c8aac6c859b8d757345ee137
Milena Santana Teixeira <milesant54@gmail.com>
2025-08-11 21:50:29 -0300

    desenha

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -3,36 +3,28 @@ import './App.css';
 
 function App() { //Aqui é JavaScript 
 
-  function calculadora(a, b, op){ //O switch não precisa do BREAK (br) ele pula um pedaço do código
-      switch(op){
-      case '+':
-        return a + b
-      case '-':
-        return a - b
-      case '*':
-        return a * b
-      case "/":
-        return a / b
+    let cesta = ['Pão', 'Suco', 'Chocolate', 'Torrone', 'Torrada'] //Só utilizado para vetor 
 
-      }
-    
+    let desenha = [];
+  
+  //declara o indice; compara se é pra continuar; incrementa o indice
+
+    for(let i=0; i<3; i++){
+      //desenha [i] = 
+      desenha.push( <p> {cesta[i]} </p> ) //não depende do [i]
+
+
     }
 
     return(
-    <main className="App">
-      
-    {calculadora(200, 10, "/")}<br/>
-    {calculadora(1200, 5, "/")}<br/>
-    {calculadora(252, 3, "-")}<br/>
-    {calculadora(325, 7, "+")}<br/>
-    {calculadora(5,calculadora(8, 7, "*"), "+")}<br/>
-    {calculadora(10,calculadora(5, 7, "-"), "+")}<br/>
+      <main className="App">
     
+      {desenha}
 
-
-    </main>
+      </main>
 
   ); 
+
 }
 
 export default App;
