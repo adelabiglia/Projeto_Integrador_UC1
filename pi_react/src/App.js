8440abe3d116242d65fc84681b292e81908ef50c
adelabiglia <adelabiglia@yahoo.com.br>
2025-08-11 22:25:16 -0300

    Feito exercicio de impares

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -15,19 +15,28 @@ function App() { // aqui é javascript
     return a /b;
   }*/
 
-  let cesta = ['pão', 'suco', 'chocolate', 'torrada', 'geléia', 'requeijão']
+  /*let cesta = ['pão', 'suco', 'chocolate', 'torrada', 'geléia', 'requeijão']
 
   let desenha = [];
   // declara o indice; compara se é para continuar; incremementa o indice//
   for(let i=1; i<4 ; i++ ){
     desenha.push(<p> {cesta[i]} </p>)
-  }
+  }*/
+  // faça uma lista com todos os numeros mas exiba apenas os numeros impares//
 
+  let numeros = [1,2,3,4,5,6,7,8,9,10]
+  let desenha = [];
+  for(let i=0; i<10; i++){
+    if(numeros[i] % 2 !=0){
+      desenha.push(<p> {numeros[i]} </p> )
+
+    }
+
+  }
+    
   return ( /* aqui é html*/
     <main className="App">
-      {
-        desenha
-      }
+      {desenha}
     </main>
   );
 }
