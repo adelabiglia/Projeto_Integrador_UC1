05b2fba8ca9f6b2ce62907c3e75e440fc2714739
adelabiglia <adelabiglia@yahoo.com.br>
2025-08-13 20:00:20 -0300

    useState

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,26 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() { //Aqui é JavaScript 
   
-  let email = ""
-  function mudaEmail(valor){
-    email = valor
-  }
+  
+  const [email, setEmail] = useState("");
+ 
+  const [senha, setSenha] = useState("");
+  
+  const [isLogin, setIslogin] = useState(true);
 
-  let senha = ""
-  function mudaSenha(valor){
-    senha = valor
-  }
   function enviar(){
     alert("Email:  " +email+ "\nSenha:  " +senha)
   }
 
-  let isLogin = false;
   
   return(/* aqui é html */
     <main className="App">
-      <button onClick={() => {isLogin = !isLogin}} >
+      <button onClick={() => setIslogin(!isLogin)} >
         {isLogin && ("Cadastrar-se")}
         {!isLogin && ("Voltar para o Login")}
       </button>
@@ -53,10 +51,10 @@ function App() { //Aqui é JavaScript
       {isLogin && (
       <form className="login">
         <label>
-          Digite Seu Email: <input type="email" name="Email" placeholder="Digite Seu Email" onChange={(e) => mudaEmail(e.target.value) } /><br/>
+          Digite Seu Email: <input type="email" name="Email" placeholder="Digite Seu Email" onChange={(e) => setEmail(e.target.value) } /><br/>
         </label>
         <label>
-          Digite sua Senha: <input type="password"  name="Senha" placeholder="Digite sua Senha" onChange={(e) => mudaSenha(e.target.value) } /><br/>
+          Digite sua Senha: <input type="password"  name="Senha" placeholder="Digite sua Senha" onChange={(e) => setSenha(e.target.value) } /><br/>
         </label>
         <button className="buttonSucess" type="submit" onClick={() => enviar () }> <strong>Salvar</strong> </button>
       </form>
