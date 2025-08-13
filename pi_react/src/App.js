d3043b35be45018098d2fba575e3bf0968f19035
GuintherBr <jeffersonguinther@gmail.com>
2025-08-13 19:27:55 -0300

    adiciona funcionalidade de login com campos de email e senha

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,21 +1,58 @@
 import logo from './logo.svg';
+import { useState } from 'react'; //useState ele retorna para gente um par variavel e funçao que quando alterado o dom mostra na tela 
 import './App.css';
 
 function App() { 
-  let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
-  let impar = []
+    let email 
 
-  for(let i = 0; i < lista.length; i++){
-    if (lista[i] % 2 != 0){
-      impar.push(lista[i])
-    }
-  }
+    let senha 
+
+function mudarEmail(valor){
+email = valor
+}
+
+function mudarSenha(valor){
+senha = valor
+}
+
+function enviar(){
+    alert("Email: " + email + " Senha: " + senha);
+}
+
+//function mudarTela(){
+  //  isLogin = !isLogin
+   // alert(isLogin)
+//}
+
+//const[] = useState() forma basica de usar o useState
+
+//let isLogin = true;
+
+const [isLogin, setIsLogin] = useState(true)
+  
   return ( 
                     
     <main className="App">
 
-       <p> impar: {impar.join(", ")} </p>
+        <button onClick= {() => setIsLogin(!isLogin)}>
+            {isLogin && ("Cadastre-se")}
+            {!isLogin && ("Voltar para Login")}
+        </button>
+
+        {!isLogin && (
+        <form className="register"></form>
+        )}
+
+        {isLogin && (
+        <form className="login">
+            <label>Login: </label>
+            <input type="email" name="Email"  placeholder="Digite seu email" value={email} onChange={(e) => mudarEmail(e.target.value)} /><br/>
+            <label>Senha: </label>
+            <input type="password" name="Senha" placeholder="Digite sua senha" value={senha} onChange={(e) => mudarSenha(e.target.value)} /><br/>
+            <button className= "buttonLogin"  onClick={() => enviar()} >Entrar</button>
+
+        </form>
+        )}   
        
     </main>
 
