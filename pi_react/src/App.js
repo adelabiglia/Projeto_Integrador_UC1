187fb8ae5e3a26b15faed94a5b901f81afedad4d
adelabiglia <adelabiglia@yahoo.com.br>
2025-08-12 22:05:21 -0300

    cadastro e inputs

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -2,22 +2,65 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() { //Aqui é JavaScript 
-
-  //Faça uma lista com todos os numeros até 10 mas exiba apenas os numeros impares
-
-  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  let mostrar = [];
-
-  for(let i=0; i < numeros.length ; i++){
-    if (numeros[i] % 2 !== 0){
-      mostrar.push( <p> {numeros[i]} </p> )
-    }
-  }
   
-  return(
+  let email = ""
+  function mudaEmail(valor){
+    email = valor
+  }
+
+  let senha = ""
+  function mudaSenha(valor){
+    senha = valor
+  }
+  function enviar(){
+    alert("Email:  " +email+ "\nSenha:  " +senha)
+  }
+
+  let isLogin = false;
+  
+  return(/* aqui é html */
     <main className="App">
-      {mostrar}
+      <button onClick={() => {isLogin = !isLogin}} >
+        {isLogin && ("Cadastrar-se")}
+        {!isLogin && ("Voltar para o Login")}
+      </button>
+
+      <br/><br/>
+  
+      {!isLogin && (
+      <form className="register" >
+        <label>
+          Nome: <input type="text" name="Nome" placeholder="Digite Seu Nome" onChange={(e) => (e.target.value) } /><br/>
+        </label>
+        <label>
+          Endereço: <input type="text" name="Endereço" placeholder="Digite Seu Endereço" onChange={(e) => (e.target.value) } /><br/>
+        </label>
+        <label>
+          Documento: <input type="text" name="Documento" placeholder="Digite Seu Documento" onChange={(e) => (e.target.value) } /><br/>
+        </label>
+        <label>
+          Cidade: <input type="text" name="Cidade" placeholder="Digite Sua Cidade" onChange={(e) => (e.target.value) } /><br/>
+        </label>
+        <label>
+          Pais: <input type="text" name="Pais" placeholder="Digite Seu Pais" onChange={(e) => (e.target.value) } /><br/>
+        </label>
+
+
+
+      </form>
+      )}
+
+      {isLogin && (
+      <form className="login">
+        <label>
+          Digite Seu Email: <input type="email" name="Email" placeholder="Digite Seu Email" onChange={(e) => mudaEmail(e.target.value) } /><br/>
+        </label>
+        <label>
+          Digite sua Senha: <input type="password"  name="Senha" placeholder="Digite sua Senha" onChange={(e) => mudaSenha(e.target.value) } /><br/>
+        </label>
+        <button className="buttonSucess" type="submit" onClick={() => enviar () }> <strong>Salvar</strong> </button>
+      </form>
+      )}     
     </main>
   ); 
 }
