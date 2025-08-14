bbe75c72482c854c290e517d229b605dc21cebea
adelabiglia <adelabiglia@yahoo.com.br>
2025-08-13 22:12:18 -0300

    Pagina de cadastro com useState e CSS

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,27 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {useState} from 'react'
 
 function App() { //Aqui é JavaScript 
+  const [isLogin, setIslogin] = useState(true);
+  
+  /*const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   const [user, setUser] = useState({
     email: "",
     password: "",
     phone: "",
     name: "",
-  });
+  });*/
   
-  //const [email, setEmail] = useState("");
-  //const [senha, setSenha] = useState("");
-  const [isLogin, setIslogin] = useState(true);
+  const [user, setUser] = useState({
+    name: "",
+    birth:"",
+    address:"",
+    number:"",
+    neighborhood: "",
+    city:"",
+    email:"",
+    password:"",
+  })
 
   function enviar(){
     alert("Email:  " +user.email+ "\nSenha:  " +user.password)
   }
-
   
   return(/* aqui é html */
     <main className="App">
-      <button onClick={() => setIslogin(!isLogin)} >
+      <button className='buttonSucess' onClick={() => setIslogin(!isLogin)} >
         {isLogin && ("Cadastrar-se")}
         {!isLogin && ("Voltar para o Login")}
       </button>
@@ -31,19 +42,22 @@ function App() { //Aqui é JavaScript
       {!isLogin && (
       <form className="register" >
         <label>
-          Nome: <input type="text" name="Nome" placeholder="Digite Seu Nome" onChange={(e) => (e.target.value) } /><br/>
+          Nome: <input type="text" name="Nome" placeholder="Digite Seu Nome" onChange={(e) => setUser({...user, name: e.target.value}) } /><br/>
         </label>
         <label>
-          Endereço: <input type="text" name="Endereço" placeholder="Digite Seu Endereço" onChange={(e) => (e.target.value) } /><br/>
+          Data de Nascimento: <input type="text" name="Data de Nascimento" placeholder="Digite Sua Data de Nascimento" onChange={(e) => setUser({...user, birth: e.target.value}) } /><br/>
         </label>
         <label>
-          Documento: <input type="text" name="Documento" placeholder="Digite Seu Documento" onChange={(e) => (e.target.value) } /><br/>
+          Endereço: <input type="text" name="Endereço" placeholder="Digite Seu Endereço" onChange={(e) => setUser({...user, address: e.target.value}) } /><br/>
         </label>
         <label>
-          Cidade: <input type="text" name="Cidade" placeholder="Digite Sua Cidade" onChange={(e) => (e.target.value) } /><br/>
+          Numero: <input type="text" name="Numero" placeholder="Digite o Número da Sua Residência " onChange={(e) => setUser({...user, number: e.target.value}) } /><br/>
         </label>
         <label>
-          Pais: <input type="text" name="Pais" placeholder="Digite Seu Pais" onChange={(e) => (e.target.value) } /><br/>
+          Bairro: <input type="text" name="Bairro" placeholder="Digite Seu Bairro" onChange={(e) => setUser({...user, neighborhood: e.target.value}) } /><br/>
+        </label>
+        <label>
+          Cidade: <input type="text" name="Cidade" placeholder="Digite Sua Cidade" onChange={(e) => setUser({...user, city: e.target.value}) } /><br/>
         </label>
 
 
@@ -57,9 +71,9 @@ function App() { //Aqui é JavaScript
           Digite Seu Email: <input type="email" name="Email" placeholder="Digite Seu Email" onChange={(e) => setUser({...user, email: e.target.value}) } /><br/>
         </label>
         <label>
-          Digite sua Senha: <input type="password"  name="Senha" placeholder="Digite sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
+          Digite Sua Senha: <input type="password"  name="Senha" placeholder="Digite Sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
         </label>
-        <button className="buttonSucess" type="submit" onClick={() => enviar () }> <strong>Salvar</strong> </button>
+        <button className="buttonSucess" type="submit" onClick={() => enviar () }> Salvar </button>
       </form>
       )}     
     </main>
