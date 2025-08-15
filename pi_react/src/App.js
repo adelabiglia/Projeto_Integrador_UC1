69478d1ea0bbae4826469caac92dda6d91a59347
adelabiglia <adelabiglia@yahoo.com.br>
2025-08-14 21:56:59 -0300

    Cadastro funcionando

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { useState } from 'react'; //useState ele retorna para gente um par variavel e funçao que quando alterado o dom mostra na tela 
 import './App.css';
+import { createClient } from "@supabase/supabase-js";
 
 
 const supabaseUrl="https://kvuxqtwfmqnookboncos.supabase.co"
@@ -10,6 +11,30 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() { //Aqui é JavaScript 
   const [isLogin, setIslogin] = useState(true);
+
+  const [isSendRegister, SetIsSendRegister] = useState(false);
+
+  const [msg, setMsg] = useState("");
+
+  async function register (){
+    SetIsSendRegister(true);
+    try{
+      let { data, error } = await supabase.auth.signUp({
+        email: user.email,
+        password: user.password
+      })
+      if(error) throw error
+      if(data.status == 400) throw data.message
+
+      setMsg("Cadastro Realizado!")
+    }catch(e){
+      setMsg(`Error: ${e.message}`)
+
+    }
+    SetIsSendRegister(false)
+
+    setTimeout(() => setMsg("") , 5000);
+  }
   
   /*const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
@@ -48,35 +73,33 @@ function App() { //Aqui é JavaScript
       {!isLogin && (
       <form className="register" >
         <label>
-          Nome: <input type="text" name="Nome" placeholder="Digite Seu Nome" onChange={(e) => setUser({...user, name: e.target.value}) } /><br/>
+          Nome: <br/><input type="text" name="Nome" placeholder="Digite Seu Nome" onChange={(e) => setUser({...user, name: e.target.value}) } /><br/>
         </label>
         <label>
-          Data de Nascimento: <input type="text" name="Data de Nascimento" placeholder="Digite Sua Data de Nascimento" onChange={(e) => setUser({...user, birth: e.target.value}) } /><br/>
+          Data de Nascimento: <br/><input type="text" name="Data de Nascimento" placeholder="Digite Sua Data de Nascimento" onChange={(e) => setUser({...user, birth: e.target.value}) } /><br/>
         </label>
         <label>
-          Endereço: <input type="text" name="Endereço" placeholder="Digite Seu Endereço" onChange={(e) => setUser({...user, address: e.target.value}) } /><br/>
+          Endereço: <br/><input type="text" name="Endereço" placeholder="Digite Seu Endereço" onChange={(e) => setUser({...user, address: e.target.value}) } /><br/>
         </label>
         <label>
-          Numero: <input type="text" name="Numero" placeholder="Digite o Número da Sua Residência " onChange={(e) => setUser({...user, number: e.target.value}) } /><br/>
+          Numero: <br/><input type="text" name="Numero" placeholder="Digite o Número da Sua Residência " onChange={(e) => setUser({...user, number: e.target.value}) } /><br/>
         </label>
         <label>
-          Bairro: <input type="text" name="Bairro" placeholder="Digite Seu Bairro" onChange={(e) => setUser({...user, neighborhood: e.target.value}) } /><br/>
+          Bairro: <br/><input type="text" name="Bairro" placeholder="Digite Seu Bairro" onChange={(e) => setUser({...user, neighborhood: e.target.value}) } /><br/>
         </label>
         <label>
-          Cidade: <input type="text" name="Cidade" placeholder="Digite Sua Cidade" onChange={(e) => setUser({...user, city: e.target.value}) } /><br/>
+          Cidade: <br/><input type="text" name="Cidade" placeholder="Digite Sua Cidade" onChange={(e) => setUser({...user, city: e.target.value}) } /><br/>
         </label>
         <label>
-          Email: <input type="text" name="Email" placeholder="Digite seu Email" onChange={(e) => setUser({...user, email: e.target.value}) } /><br/>
+          Email: <br/><input type="text" name="Email" placeholder="Digite seu Email" onChange={(e) => setUser({...user, email: e.target.value}) } /><br/>
         </label>
         <label>
-          Senha: <input type="password" name="Senha" placeholder="Digite Sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
+          Senha: <br/><input type="password" name="Senha" placeholder="Digite Sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
         </label>
-
-
-
-                    </form>
-                )
-            }
+        <button className="buttonSucess" onClick={register} disabled={isSendRegister} > {isSendRegister ? "Cadastrando..." : "Cadastrar"} </button>
+      </form>
+      )
+      }
 
       {isLogin && (
       <form className="login">
@@ -86,9 +109,11 @@ function App() { //Aqui é JavaScript
         <label>
           Digite Sua Senha: <input type="password"  name="Senha" placeholder="Digite Sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
         </label>
-        <button className="buttonSucess" type="submit" onClick={() => enviar () }> Salvar </button>
+        <button className="buttonSucess" type="submit" onClick={register}> Salvar </button>
       </form>
-      )}     
+      )}
+
+      {msg && (<div className='toast'>{msg} </div>)}     
     </main>
   ); 
 }
