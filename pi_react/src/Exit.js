2285f09019de93b4ea15847e2b204c55e1e1d4b3
adelabiglia <adelabiglia@yahoo.com.br>
2025-08-20 21:18:29 -0300

    criação objeto exit

--- pi_react/src/Exit.js
+++ pi_react/src/Exit.js
@@ -6,116 +6,29 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl="https://kvuxqtwfmqnookboncos.supabase.co"
 const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2dXhxdHdmbXFub29rYm9uY29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTA4NjIsImV4cCI6MjA2OTkyNjg2Mn0.n2F4uWJuIxu17qjEfHHFmv3Kg9uq5con54ys3E3Al9g"
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function Exit() { //Aqui é JavaScript 
-  const [isLogin, setIslogin] = useState(true);
-
-  const [isSendRegister, SetIsSendRegister] = useState(false);
-
-  const [msg, setMsg] = useState("");
-
-  async function register (){
-    SetIsSendRegister(true);
-    try{
-      let { data, error } = await supabase.auth.signUp({
-        email: user.email,
-        password: user.password
-      })
-      if(error) throw error
-      if(data.status == 400) throw data.message
-
-      setMsg("Cadastro Realizado!")
-    }catch(e){
-      setMsg(`Error: ${e.message}`)
-
-    }
-    SetIsSendRegister(false)
-
-    setTimeout(() => setMsg("") , 5000);
-  }
+  const [exit, setExit] = useState ({
+    date:"",
+    description:"",
+    value:"",
+    category_id: "",
+    user_id: "",
+  })  
   
-  /*const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-    phone: "",
-    name: "",
-  });*/
-  
-  const [user, setUser] = useState({
-    name: "",
-    birth:"",
-    address:"",
-    number:"",
-    neighborhood: "",
-    city:"",
-    email:"",
-    password:"",
-  })
-
-  function enviar(){
-    alert("Email:  " +user.email+ "\nSenha:  " +user.password)
-  }
-  
-  return(/* aqui é html */
-    <main className="App">
-      <button className='buttonSucess' onClick={() => setIslogin(!isLogin)} >
-        {isLogin && ("Cadastrar-se")}
-        {!isLogin && ("Voltar para o Login")}
-      </button>
-
-      <br/><br/>
-  
-      {!isLogin && (
-      <form className="register" >
-        <label>
-          Nome: <br/><input type="text" name="Nome" placeholder="Digite Seu Nome" onChange={(e) => setUser({...user, name: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Data de Nascimento: <br/><input type="text" name="Data de Nascimento" placeholder="Digite Sua Data de Nascimento" onChange={(e) => setUser({...user, birth: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Endereço: <br/><input type="text" name="Endereço" placeholder="Digite Seu Endereço" onChange={(e) => setUser({...user, address: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Numero: <br/><input type="text" name="Numero" placeholder="Digite o Número da Sua Residência " onChange={(e) => setUser({...user, number: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Bairro: <br/><input type="text" name="Bairro" placeholder="Digite Seu Bairro" onChange={(e) => setUser({...user, neighborhood: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Cidade: <br/><input type="text" name="Cidade" placeholder="Digite Sua Cidade" onChange={(e) => setUser({...user, city: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Email: <br/><input type="text" name="Email" placeholder="Digite seu Email" onChange={(e) => setUser({...user, email: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Senha: <br/><input type="password" name="Senha" placeholder="Digite Sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
-        </label>
-        <button className="buttonSucess" onClick={register} disabled={isSendRegister} > {isSendRegister ? "Cadastrando..." : "Cadastrar"} </button>
+  retunr (
+    <div className="screen">
+      <form>
+        <input type='date' placeholder='Data' onChange={(e) => setExit ({...exit, date: e.target.value})} />
+        <input type='text' placeholder='Descrição' onChange={(e) => setExit ({...exit, description: e.target.value})}/>
+        <input type='number' placeholder='Valor' onChange={(e) => setExit ({...exit, value: e.target.value})}/>
+        <input type='text' placeholder='essa é chave da categoria' onChange={(e) => setExit ({...exit, category_id: e.target.value})}/>
       </form>
-      )
-      }
+    </div>
 
-      {isLogin && (
-      <form className="login">
-        <label>
-          Digite Seu Email: <input type="email" name="Email" placeholder="Digite Seu Email" onChange={(e) => setUser({...user, email: e.target.value}) } /><br/>
-        </label>
-        <label>
-          Digite Sua Senha: <input type="password"  name="Senha" placeholder="Digite Sua Senha" onChange={(e) => setUser({...user, password: e.target.value}) } /><br/>
-        </label>
-        <button className="buttonSucess" type="submit" onClick={register}> Salvar </button>
-      </form>
-      )}
-
-      {msg && (<div className='toast'>{msg} </div>)}     
-    </main>
-  ); 
+  );
+   
 }
 
 export default Exit;
