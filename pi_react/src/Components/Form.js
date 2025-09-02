3b6dfbd5b9be10f4e48d8c83b0cd6ac2fd4e2d52
Milena Santana Teixeira <milesant54@gmail.com>
2025-09-02 20:05:02 -0300

    alteração

---
+++ pi_react/src/Components/Form.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+
+
+function Form({
+    onSubmit,
+    func,
+    title,
+    children,
+     
+}){
+
+    const [loading, setLoading] = useState(false);
+
+    async function send(){
+         setLoading(true)
+         var any = await func()
+         setLoading(false)
+         return any
+    }
+
+    return(
+        < div className='cardForm'>
+            <h3>{title}</h3>
+            <form onSubmit={onSubmit}>
+                {children}
+
+
+                <Button onClick={() => send()} disabled={loading}>
+                     {loading ? (
+                        <Spinner animation="border" role="status">
+                                <span className="visually-hidden">
+                                Loading...
+                                </span>
+                        </Spinner>
+
+                    ):(
+                        <>Salvar</>
+
+                    )}
+                </Button>
+
+        </form>
+        </div>
+
+    );
+
+}
+
+export {Form}   
+
